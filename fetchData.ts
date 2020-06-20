004ab2f5be4fce93e9e994f2db4ae6a4eebd8672
Rahul Sahay <rahulsahay19@hotmail.com>
2020-06-20 13:47:10 +0530

    sample stuff

--- fetchData.ts
+++ fetchData.ts
@@ -1,12 +1,49 @@
-// import * as log from 
-async function downloadLaunchData() {
-   const response = await fetch('https://api.spacexdata.com/v3/launches', {
-        method: 'GET'
-    });
-
-// since its returning prmoise, hence promise applied
-const launchData = await response.json();
-console.log(launchData);
+import * as log from "https://deno.land/std/log/mod.ts";
+import * as _ from "https://raw.githubusercontent.com/lodash/lodash/4.17.15-es/lodash.js";
+interface Launch {
+  flightNumber: number;
+  mission: string;
+  rocket: string;
+  customers: Array<string>;
 }
 
-await downloadLaunchData();
+const launches = new Map<number, Launch>();
+export async function downloadLaunchData() {
+  log.info("Downloading launch data");
+  const response = await fetch("https://api.spacexdata.com/v3/launches", {
+    method: "GET",
+  });
+
+  if (!response.ok) {
+    log.warning("Problem downloading launch data");
+  } else {
+    log.info("Downloaded launch data");
+  }
+  // since its returning prmoise, hence promise applied
+  const launchData = await response.json();
+  //console.log(launchData);
+  for (const launch of launchData) {
+
+    const payloads = launch["rocket"]["second_stage"]["payloads"];
+   // console.log(payloads);
+    const customers = _.flatMap(payloads, (payload: any) => {
+        return payload["customers"];
+    });
+    //console.log(customers);
+    const flightData = {
+      flightNumber: launch["flight_number"],
+      mission: launch["mission_name"],
+      rocket: launch["rocket"]["rocket_name"],
+      customers: customers
+    };
+    launches.set(flightData.flightNumber, flightData);
+    log.info(JSON.stringify(flightData));
+  }
+}
+
+if(import.meta.main) {
+    await downloadLaunchData();
+    // true means its running from the same file. false means its imported via some module
+    log.info(JSON.stringify(import.meta));
+    log.info(`Downloaded data for ${launches.size} SpaceX launches`);
+}
